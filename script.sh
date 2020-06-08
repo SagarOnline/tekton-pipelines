4eea3ea7a96ee5875251fdf374e31c18a7de14ee
schopade <schopade@in.ibm.com>
2020-06-08 15:08:14 +0530

    updated script

--- script.sh
+++ script.sh
@@ -1,3 +1,12 @@
 #!/bin/bash
 ls -lrst
 oc version
+
+read -p "Do you wish to setup pipelines in Test Environments for Release '$releaseName'? : " yn
+case $yn in
+	[Yy]* ) 
+		echo "Creating pipelines in Test environments for Release '$releaseName' ....." 
+		break;;
+	[Nn]* ) break;;
+	* ) echo "Please answer y or n.";;
+esac
